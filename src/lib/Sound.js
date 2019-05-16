bb49622616eabcb21d7746e1cb0b62ec45dc9600
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2019-05-16 17:52:39 +0200

    Sound engine improved

--- src/lib/Sound.js
+++ src/lib/Sound.js
@@ -1,23 +1,23 @@
 /* NOTE: This is a .js file because TypeScript doesn't like the window object */
 
+const channels = 4;
+
 export default class Sound {
 
   constructor() {
     window.AudioContext = window.AudioContext || window.webkitAudioContext || {};
     this.index = 0;
     this.sources = {};
-    this.buffers = [
-      new Audio(),
-      new Audio(),
-      new Audio(),
-      new Audio()
-    ];
+    this.buffers = new Array(channels);
+    for (let i = 0; i < this.buffers.length; i++) this.buffers[i] = new Audio();
+    console.log(this.buffers);
   }
 
   play(name) {
-    this.index = (this.index + 1) % this.buffers.length;
+    console.log(this.index);
     this.buffers[this.index].src = this.buffers[name].src;
     this.buffers[this.index].play();
+    this.index = (this.index + 1) % this.buffers.length;
   }
 
   registerSample(key, data) {
