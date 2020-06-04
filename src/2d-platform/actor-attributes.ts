3fc141768158b4adcdc3356cc87eb610fb7c0438
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2020-06-04 13:56:36 +0200

    With refactor we don't need to split out movement and condition determination for 2d

--- src/2d-platform/actor-attributes.ts
+++ src/2d-platform/actor-attributes.ts
@@ -2,7 +2,6 @@ import NoProgress from '../resources/NoProgress';
 import BasicPlatformPlayerDirectionProgress from '../resources/BasicPlatformPlayerDirectionProgress';
 import NoStateChange from '../resources/NoStateChange';
 import SarahStateChanger from './SarahStateChanger';
-import BasicPlatformPlayerConditionProgress from '../resources/BasicPlatformPlayerConditionProgress';
 import PositionCamera from '../resources/PositionCamera';
 
 const pixelsPerSecond = 300;
@@ -17,7 +16,6 @@ export default {
   'actor-1': {
     progress: [
       PlayerProgress,
-      // BasicPlatformPlayerConditionProgress,
       PositionCamera
     ],
     stateChanger: SarahStateChanger
