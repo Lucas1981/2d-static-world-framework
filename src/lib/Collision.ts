75bd8725886edc04020b4087b43ad59e4de6eb22
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2021-11-07 13:19:45 +0100

    Fixed a very old and very core off-by-one error in the Collision Detection

--- src/lib/Collision.ts
+++ src/lib/Collision.ts
@@ -3,7 +3,6 @@ import Frame from './Frame';
 import global from './Global';
 
 export default class Collision {
-
   constructor() {}
 
   public static isColliding(source: Actor, target: Actor): boolean {
@@ -17,19 +16,18 @@ export default class Collision {
   }
 
   private static squareBoxCollision(sourcePlayer: Actor, targetPlayer: Actor): boolean {
-
     const source = sourcePlayer.getCurrentBox();
     const target = targetPlayer.getCurrentBox();
 
     const sx1 = source.x;
     const sy1 = source.y;
-    const sx2 = sx1 + source.width;
-    const sy2 = sy1 + source.height;
+    const sx2 = sx1 + (source.width - 1);
+    const sy2 = sy1 + (source.height - 1);
 
     const dx1 = target.x;
     const dy1 = target.y;
-    const dx2 = dx1 + target.width;
-    const dy2 = dy1 + target.height;
+    const dx2 = dx1 + (target.width - 1);
+    const dy2 = dy1 + (target.height - 1);
 
     return
     (
@@ -53,9 +51,7 @@ export default class Collision {
     );
   }
 
-
   private static pixelCollision(sourcePlayer: Actor, targetPlayer: Actor): boolean {
-
     let source: any = sourcePlayer.getCurrentBox();
     let target: any = targetPlayer.getCurrentBox();
     let sourceFrame: Frame = sourcePlayer.getCurrentFrame();
